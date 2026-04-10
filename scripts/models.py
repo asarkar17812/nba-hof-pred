907818b31dd5e45534366cfbdbedeeb22c77701d
Ayush <as17812@nyu.edu>
2026-04-10 09:40:18 -0400

    basic file tree setup

---
+++ scripts/models.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.utils.data import DataLoader
+
